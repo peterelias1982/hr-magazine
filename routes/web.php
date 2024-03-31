12867c3b70e7f520f9c5b1364b9e359f6906b264
susanasameh <susana.sameh.2692@gmail.com>
2024-03-31 14:49:49 +0200

    article routes

--- routes/web.php
+++ routes/web.php
@@ -7,4 +7,20 @@ Route::get('/', function () {
     return view('Admin.article.addArticle');
 });
 
+Route::prefix('admin')->group(function(){
+
+    // article routes
+
+    Route::get('/articles',[ArticleController::class,'index'])->name('articles.index');
+
+    Route::get('articles/create',[ArticleController::class,'create'])->name('articles.create');
+
+    Route::post('articles/',[ArticleController::class,'store'])->name('articles.store');
+
+    Route::get('articles/{slug}',[ArticleController::class,'show'])->name('articles.show');
+
+    Route::put('articles/{slug}',[ArticleController::class,'update'])->name('articles.update');
+
+    Route::delete('articles/{slug}',[ArticleController::class,'destroy'])->name('articles.destroy');
+    });
 
