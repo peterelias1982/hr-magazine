cfc10a5a721301b6d6f8cd0184d7456b1e6868fc
MonaGalal <m_gf94@hotmail.com>
2024-05-02 15:45:33 +0300

    comments

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Session;
+use App\Http\Controllers\Admin\CommentsController;
 
 
 Route::get('/', function () {
@@ -17,11 +18,12 @@ Route::get('/home', function () {
 Route::get('category/{category}/article/{article}', function ($category, $article) {
     $categoryData = \App\Models\ArticleCategory::where('slug', $category)->first();
     $articleData = \App\Models\Article::where('slug', $article)->first();
+    $comment = \App\Models\Comment::where('article_id', $articleData->id)->get();
 
 //    if article.category_id not equal category.id abort
     return view('publicPages.articles.articleSingle', compact('categoryData', 'articleData'));
 });
-
+ 
 Auth::routes();
 
 
